534761af2a0efb66b171a918fd82b3cbb230e7af
Ryclos <loogle@hotmail.com>
2018-03-14 01:16:03 -1000

    #5 Modif Mineur

--- js/identifiant.js
+++ js/identifiant.js
@@ -3,6 +3,7 @@
 */
 var prenom = prompt("Entrez votre prénom :");
 console.log("Bonjour, " + prenom);
+
 var saisie = prompt("Entrez un nombre : "); // saisie est de type chaîne
 var nb = Number(saisie); // nb est de type nombre
 // Ou var saisie = Number(prompt("Entrez un nombre : "); 
