cdf117859958216a26d3ea402c450d7bc5de0005
Ryclos <loogle@hotmail.com>
2018-03-14 00:11:14 -1000

    #4 Modif mineure

---
+++ js/identifiant.js
@@ -0,0 +1,8 @@
+/* 
+* Interraction with User
+*/
+var prenom = prompt("Entrez votre prénom :");
+console.log("Bonjour, " + prenom);
+var saisie = prompt("Entrez un nombre : "); // saisie est de type chaîne
+var nb = Number(saisie); // nb est de type nombre
+// Ou var saisie = Number(prompt("Entrez un nombre : "); 
