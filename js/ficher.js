6ae075622e1cbd4afe40ad8361ac304457f9152c
Ryclos <loogle@hotmail.com>
2018-03-17 23:36:39 -1000

    #8 Push et essai JS

---
+++ js/ficher.js
@@ -0,0 +1,26 @@
+
+var name = prompt("Bonjour, comment tu t'appelles ?");
+alert("Bienvenue " +name+ "! Ton nom est composé de " +name.length+ " lettres");
+
+var age = Number(prompt("Quel âge as-tu ?"));
+if (age === String) {
+    age = age.toString();
+}
+console.log("Tu as donc " +age+ " ans");
+//Mettre un nombre/chiffre String en Int
+
+var chiffreStrF = "12.63"
+var chiffreStr = "12";
+var chiffre = 45;
+var chiffreToStr = chiffre.toString();
+var chiffreInt = parseInt(chiffreStr);
+var chiffreFloat = parseFloat(chiffreStrF);
+console.log(chiffreInt);
+console.log(chiffreStr);
+console.log(chiffreFloat);
+console.log(chiffreToStr);
+
+//L'emplacement de telle ou telle lettre
+
+var nom = "Salut Hiomai";
+console.log("La lettre commence à cette case : " +nom.indexOf("Hiomai"));
